14515b3f39e98a44d546597693c370db1c718f43
s3794216 <s3794216@student.rmit.edu.au>
2024-09-11 20:16:37 +1000

    Add initial mvc project structure with Flask setup
    Implemented the initial project structure, including models, controllers, templates, and static files. Added sample user and job function to display how app will be built. Added instructions in the README for running the local instance.

---
+++ app/controllers/job_controller.py
@@ -0,0 +1,14 @@
+# Sample controller
+
+from flask import Blueprint, render_template
+from app.models.job import Job
+
+jobs = Blueprint('jobs', __name__)
+
+@jobs.route('/jobs')
+def list_jobs():
+    jobs = [
+        Job("Software Engineer", "Develop web applications", "Tech Co", "Melbourne", "$100,000"),
+        Job("Data Analyst", "Analyze business data", "Data Corp", "Sydney", "$80,000")
+    ]
+    return render_template('jobs.html', jobs=jobs)
