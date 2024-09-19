e3515e3255bfb9e8408f961ec6ff0aa96d372194
Nikhil Govula <s3900665@student.rmit.edu.au>
2024-09-19 14:44:47 +1000

    Login pages finished- MVP level

--- src/__init__.py
+++ src/__init__.py
@@ -2,10 +2,19 @@ from flask import Flask
 from .views.job_views import jobs
 from .views.index_views import indexs
 from .views.login_views import logins
+import os
+print("Template folder path:", os.path.abspath('src/templates'))
 
 def create_app():
-    application = app = Flask(__name__)
+    # Specify the template folder explicitly
+    template_dir = os.path.abspath('src/templates')
+    application = Flask(__name__, template_folder=template_dir)
+
+    application.config['TEMPLATES_AUTO_RELOAD'] = True  # Enable template auto-reload
+
+    # Register blueprints
     application.register_blueprint(jobs)
     application.register_blueprint(indexs)
     application.register_blueprint(logins)
+
     return application
