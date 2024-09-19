4fca9b217abf7f8d24506550963bf6716a53315d
Nikhil Govula <s3900665@student.rmit.edu.au>
2024-09-19 14:12:28 +1000

    Login base

--- src/views/login_views.py
+++ src/views/login_views.py
@@ -1,24 +1,23 @@
-from flask import Blueprint, render_template, redirect
-# from ..controllers.login_controller import get_user
+from flask import Blueprint, render_template, redirect, request
 
 logins = Blueprint('logins', __name__)
 
 @logins.route('/Login')
 def index_user():
-        return render_template('login_user.html')
+    return render_template('user/login_user.html')
 
 @logins.route('/Admin/Login')
 def index_admin():
-        return render_template('login_admin.html')
-    
+    return render_template('admin/login_admin.html')
+
 @logins.route('/Admin/')
 def redirect_admin():
-    return redirect("/Admin/Login", code=302)    
-    
+    return redirect("/Admin/Login", code=302)
+
 @logins.route('/Company/Login')
 def index_company():
-        return render_template('login_admin.html')
-    
+    return render_template('company/login_company.html')
+
 @logins.route('/Company/')
 def redirect_company():
     return redirect("/Company/Login", code=302)
